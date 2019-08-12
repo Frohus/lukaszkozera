d37f177dc81185a0f1279e5d5e99f0be87fdd120
Lukasz <37832788+Frohus@users.noreply.github.com>
2019-08-12 06:57:05 +0100

    Update urls.py

--- lukaszkozera/urls.py
+++ lukaszkozera/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('a5i10x25nnmm2/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('home_page.urls')),
 ]
